e36389db447cda4ca958f102e4cf72dfe030207a
Selim Agovic <selimagovic@gmail.com>
2021-05-27 22:14:47 +0200

    Initial Commit of Motocross2D

---
+++ Motocross2D/Assets/Scripts/Motor Scripts/ScoreScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Text scoreText;
+
+    public static int score = 0;
+    void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+
+}
---
+++ Motocross2D/Assets/Scripts/Scene Scripts/Triggers.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Triggers : MonoBehaviour
+{
+    public Slider slider;
+    public int value;
+    GameObject[] gameObjects;
+    void Start()
+    {
+        slider.enabled = false;
+        gameObjects = GameObject.FindGameObjectsWithTag("GameOver");
+    }
+
+    void OnTriggerEnter2D()
+    {
+        slider.enabled = true;
+        slider.value = value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value -= Time.deltaTime;
+    }
+    void OnTriggerExit2D()
+    {
+        slider.enabled = false;
+    }
+}
