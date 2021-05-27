a0e33e04ed3209c9d2d3891c20d18fb1b3d34e4c
Selim Agovic <selimagovic@gmail.com>
2021-05-27 21:11:39 +0200

    Initial Commit of Motocross2D

---
+++ Motocross2D/CG Proje v1.1/Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour 
+{
+	public Canvas exitMenu;
+	public Canvas optionsMenu;
+    public Canvas creditsMenu;
+	public Button startButton;
+	public Button optionsButton;
+	public Button exitButton;
+	public Button xButton;
+    public Button creditsButton;
+	// Use this for initialization
+	void Start () 
+	{
+        Time.timeScale = 1;
+        creditsMenu = creditsMenu.GetComponent<Canvas>();
+		exitMenu = exitMenu.GetComponent<Canvas> ();
+		optionsMenu = optionsMenu.GetComponent<Canvas> ();
+		startButton = startButton.GetComponent<Button> ();
+		optionsButton = optionsButton.GetComponent<Button> ();
+		exitButton = exitButton.GetComponent<Button> ();
+		xButton = xButton.GetComponent<Button> ();
+        creditsButton = creditsButton.GetComponent<Button>();
+        exitMenu.enabled = false;
+		optionsMenu.enabled = false;
+        creditsMenu.enabled = false;
+	}
+	public void xButtonPress()
+	{
+		exitMenu.enabled = false;
+		optionsMenu.enabled = false;
+		startButton.enabled= true;
+		optionsButton.enabled = true;
+		exitButton.enabled = true;
+        creditsButton.enabled = true;
+        creditsMenu.enabled = false;
+	}
+    void OnEnable()
+    {
+
+    }
+    void OnDisable()
+    {
+
+    }
+    void OnValidate()
+    {
+
+    }
+	public void ExitPress()
+	{
+		exitMenu.enabled = true;
+		optionsMenu.enabled = false;
+		startButton.enabled=false;
+		optionsButton.enabled = false;
+		exitButton.enabled = false;
+        creditsButton.enabled = false;
+	}
+
+	public void OptionsPress ()
+	{
+		exitMenu.enabled = false;
+		optionsMenu.enabled = true;
+		startButton.enabled=false;
+		optionsButton.enabled = false;
+        creditsMenu.enabled = false;
+		exitButton.enabled = false;
+        creditsButton.enabled = false;
+	}
+    public void CreditsPress()
+    {
+        exitMenu.enabled = false;
+        optionsMenu.enabled = false;
+        startButton.enabled = false;
+        optionsButton.enabled = false;
+        creditsMenu.enabled = true;
+        exitButton.enabled = false;
+        creditsButton.enabled = false;
+    }
+
+    public void StartLevel()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void NoPress()
+	{
+		exitMenu.enabled = false;
+		startButton.enabled= true;
+		optionsButton.enabled = true;
+		exitButton.enabled = true;
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+
+}
---
+++ Motocross2D/CG Proje v1.1/Assets/Scripts/Menu/PauseUI.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
+public class PauseUI : MonoBehaviour
+{
+    GameObject[] pauseObjects;
+    GameObject[] gameOverObjects;
+    public GameObject rain;
+    public static int deger = 1;
+    
+    void Awake()
+    {
+        deger = 1;
+        Time.timeScale = 1;
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        HideOnPause();	
+	}
+
+    static Worker workerObject = workerObject = new Worker();
+    Thread workerThread = new Thread(workerObject.DoWork);
+
+    public void Resume()
+    {
+        HideOnPause();
+        workerThread = new Thread(workerObject.DoWork);
+        workerThread.Start();
+    }
+
+    public void Reload()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        
+        SceneManager.LoadScene(scene.name);
+    }
+    //hides objects show in tag
+    public void HideOnPause()
+    {
+        foreach (GameObject g in pauseObjects)
+        { g.SetActive(false); }
+    }
+    public void ShowOnPause()
+    {
+        foreach (GameObject g in pauseObjects)
+        { g.SetActive(true); }
+    }
+    // Update is called once per frame
+
+    public void MainScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Time.timeScale == 1)
+            {
+                deger = 0;
+                
+                ShowOnPause();
+            }
+            else if(Time.timeScale == 0)
+            {
+                HideOnPause();
+                workerThread = new Thread(workerObject.DoWork);
+                workerThread.Start();
+            }
+        }
+        Time.timeScale = deger;
+    }    
+}
+
+internal class Worker
+{
+    public void DoWork()
+    {
+        Thread.Sleep(1000);
+        PauseUI.deger = 1;
+    }
+}
